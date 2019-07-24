78459e40663e45b266e9c04a60e8dfad544c07bc
Ajay Reddy <ajayreddy0112@gmail.com>
2019-07-24 11:02:44 +0530

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+//Weather or forecast api Darksky.net
+const forecast = (lat, long, callback) => {
+    const url = 'https://api.darksky.net/forecast/685dce9cbed2a11aa57a828b1445d36d/' + lat + ',' + long + '?units=si'
+
+    request({ url, json: true }, (error, {body}) => {
+
+        if (error) {
+            callback('Unable to connect to weather services!',undefined);
+        } else if (body.error) {
+            callback('Unable to find location!',undefined);
+        } else {
+            const temperature = body.currently.temperature;
+            const precipProbability = body.currently.precipProbability
+            const summary = body.daily.data[0].summary
+            callback(undefined,`${summary} It is currently ${temperature} degree out. There is a ${precipProbability}% chance of rain.`);
+        }
+    })
+}
+
+module.exports = forecast
